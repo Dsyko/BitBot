a28db116a1fa4d596408e165239d7d2e953b6015
Dsyko <dsykora@gmail.com>
2013-04-04 02:59:56 -0700

    Fixing some python issues and URL for MtGox
    Signed-off-by: Dsyko <dsykora@gmail.com>

--- MtGox.py
+++ MtGox.py
@@ -1,5 +1,6 @@
 __author__ = 'dsyko'
 
+#import libraries we'll need
 from urllib import urlencode
 import urllib2
 import time
@@ -7,6 +8,8 @@ from hashlib import sha512
 from hmac import HMAC
 import base64
 import json
+
+#We need a nonce(one time use number to prevent repeat-requests and copycat attacks) for each API request
 def get_nonce():
     return int(time.time()*100000)
 
@@ -17,18 +20,19 @@ class requester:
     def __init__(self, auth_key, auth_secret):
         self.auth_key = auth_key
         self.auth_secret = base64.b64decode(auth_secret)
+        self.base = "https://data.mtgox.com/api/2/"
 
-    def build_query(self, req={}):
-        req["nonce"] = get_nonce()
-        post_data = urlencode(req)
-        headers = {}
-        headers["User-Agent"] = "GoxApi"
-        headers["Rest-Key"] = self.auth_key
-        headers["Rest-Sign"] = sign_data(self.auth_secret, post_data)
-        return (post_data, headers)
+    def build_query(self, request=None):
+        if not request: request = {}
+        request["nonce"] = get_nonce()
+        post_data = urlencode(request)
+        headers = {"User-Agent": "BitBot",
+                   "Rest-Key": self.auth_key,
+                   "Rest-Sign": sign_data(self.auth_secret, post_data)}
+        return post_data, headers
 
     def perform(self, path, args):
         data, headers = self.build_query(args)
-        req = urllib2.Request("https://mtgox.com/api/0/"+path, data, headers)
-        res = urllib2.urlopen(req, data)
-        return json.load(res)
+        request = urllib2.Request(self.base + path, data, headers)
+        response = urllib2.urlopen(request, data)
+        return json.load(response)
