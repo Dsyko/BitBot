edfb9f22aca24e78d9b7aaefe79d257f0144efd7
David Sykora <dsykora@gmail.com>
2013-05-04 22:47:04 -0700

    Generic Secret.py
    This is what your Secret.py should look like…

---
+++ Secret.py
@@ -0,0 +1,8 @@
+__author__ = 'dsyko'
+#Our API key and secret for MtGox don't put this in source control or on GitHub
+gox_api_key = "Your API Key Here"
+gox_auth_secret = "Your Auth Secret Here"
+couch_url = 'https://user:pass@couchDBurl.com:6984'
+bitcoin_historic_data_db_name = "bitcoin-historic-data"
+bitcoin_historic_data_view_name = "Prices/time"
+logging_db_name = "bitbot-logs"
